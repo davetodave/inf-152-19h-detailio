23ceed0def453a1e65824b25f06db90b688a32de
davetodave <dave-iadeluca@bluewin.ch>
2022-06-17 12:25:56 +0200

    feat: add scroll button in header image section

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 (function($){
 
     $(document).ready(function() {
-   		  
+   		 
+   		$("a[href$='scroll-to-anchor']").click(function() {
+        scrollToContent();
+
+        return false;
+        });
+
+       function scrollToContent() {
+
+        var objDeferred = $.Deferred();
+        var scrollOffset = window.innerHeight;
+
+        $('html, body').animate({ scrollTop: scrollOffset }, 1000);
+
+        scroll = false;
+
+        setTimeout(function() {
+            objDeferred.resolve();
+        }, 1030);
+
+        return objDeferred;
+        } 
         
     });
 
